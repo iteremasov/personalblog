2fd2e3e00c01a2421db7df92b9df43aee6d28759
iteremasov <teremasovjr@gmail.com>
2018-05-29 00:49:50 +0600

    Create object Article

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 
-# Create your models here.
 
+class Articles(models):
+    def title(self):
+        title = "010101"
+        return title
 
+    def description(self):
+        descriptions = "010101"
+
+        return descriptions
+
+    def body(self):
+        body = "10101"
+        return body
+
+    def date(self):
+        date = "101010"
+        return date
+
+    def category(self, category):
+        category = "010101"
+        return category
+    # Create your models here.
