6afe8b4269322c53204ecabbd2109452f7180e30
vteremasov <vteremasov@klika-tech.com>
2018-05-29 20:45:33 +0300

    Added requirements.txt Added simple controller with random gif

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+import giphypop
 
 from .models import Articles
 
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
+giphy = giphypop.Giphy()
 
 def index(request):
     articles = Articles.objects.order_by('-date')[:5]
@@ -23,4 +26,8 @@ def results(request, article_id):
 def vote(request, article_id):
     return HttpResponse("You're voting on question %s." % article_id)
 
+
 # Create your views here.
+def rgif(request):
+    rgif = giphy.screensaver('funny')
+    return TemplateResponse(request, 'rgif.html', {'source': rgif.id})
